168c6ba30195f4fa2f84271c22cd01481917ddb8
A.Loo <loo_allan@yahoo.com>
2020-12-08 11:36:15 +0200

    Tasks 1-3, bit of a issue with current user id always being 1.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -25,19 +25,73 @@ router.get('/', authorize, (request, response) => {
 router.post('/', authorize,  (request, response) => {
 
     // Endpoint to create a new post
+    let text = request.body.text;
+    let media = request.body.media;
+
+    let form = {
+        text: {required: false},
+        media: {required: false},
+    };
+
+    const fieldMissing = {
+        code: null,
+        message: 'Please provide %s field'
+    };
+
+    for (let field in form) {
+        if (!media.type) {
+            if (media.url) {
+                fieldMissing.code = field;
+                fieldMissing.message = fieldMissing.message.replace('%s', field);
+                return;
+            } else {
+                if (!text) {
+                    return;
+                }
+            }
+        } else {
+            if (!media.url) {
+                fieldMissing.code = field;
+                fieldMissing.message = fieldMissing.message.replace('%s', field);
+                return;
+            }
+        }
+    }
+
+    let params = {
+        userId: request.currentUser.id,
+        text: request.body.text,
+        media: request.body.media
+    };
+
+    PostModel.create(params, () => {
+        response.status(201).json()
+    });
 
 });
 
 
 router.put('/:postId/likes', authorize, (request, response) => {
-
     // Endpoint for current user to like a post
+    let userId = request.currentUser.id;
+    let postId = request.params.postId;
+    console.log(userId)
+    console.log(postId)
+
+    PostModel.like(userId, postId , () => {
+        response.status(200).json()
+    })
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
-
     // Endpoint for current user to unlike a post
-
+    let userId = request.currentUser.id;
+    let postId = request.params.postId;
+    console.log(userId)
+    console.log(postId)
+    PostModel.unlike(userId, postId, () => {
+        response.status(200).json()
+    })
 });
 
 module.exports = router;
